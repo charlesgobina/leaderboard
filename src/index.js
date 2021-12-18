b26fc7bac70efab29d721edbf0a02d350fcaf301
charlesgobina <charles.gobina@gmail.com>
2021-12-18 03:14:53 +0100

    Implement API functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 import './style.css';
+import GameManager from './gameman.js';
+import { domVars } from './domvar.js';
+
+const gameMan = new GameManager();
+
+gameMan.name = domVars.name;
+gameMan.score = domVars.score;
+
+function showResults() {
+  const res = gameMan.displayScore();
+  res.then((finalResponse) => {
+    GameManager.renderer(finalResponse, domVars.wrap);
+    domVars.refresh.addEventListener('click', () => {
+      GameManager.renderer(finalResponse, domVars.wrap);
+      window.location.reload();
+    });
+  });
+}
+
+function submit() {
+  domVars.scan.addEventListener('submit', (e) => {
+    e.preventDefault();
+    gameMan.addScore(gameMan.name.value, gameMan.score.value);
+    gameMan.name.value = '';
+    gameMan.score.value = '';
+    showResults();
+  });
+}
+
+window.onload = () => {
+  showResults();
+  submit();
+};
